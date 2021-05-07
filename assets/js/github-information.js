5e4671bc3719031e1c6e8f907f9a993a65a29739
ShiftyKitty <cathalpmcgahan@gmail.com>
2021-05-07 19:23:42 +0000

    added better api overload response to github section

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -37,6 +37,8 @@ function repoInformationHTML(repos) {
 }
 
 function fetchGitHubInformation(event) {
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
 
     var username = $("#gh-username").val();
     if (!username) {
@@ -63,6 +65,10 @@ function fetchGitHubInformation(event) {
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(
                     `<h2>No info found for user ${username}</h2>`);
+            } else if(errorResponse.status === 403) {
+                var resetTime = new Date(errorResponse.getResponseHeader("X-RateLimit-Reset")*1000);
+                $("#gh-user-data").html(`<h4>She's gonna blow, please wait until ${resetTime.toLocaleTimeString()} </h4>`);
+
             } else {
                 console.log(errorResponse);
                 $("#gh-user-data").html(
@@ -70,3 +76,5 @@ function fetchGitHubInformation(event) {
             }
         });
 }
+
+$(document).ready(fetchGitHubInformation);
